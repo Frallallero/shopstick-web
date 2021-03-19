2baee3a213ba866996c5db41fe8901d7a9619f73
mario.bianco <mario.bianco@accenture.com>
2021-03-19 23:59:08 +0100

    first message

---
+++ README.md
@@ -0,0 +1,1 @@
+"# shopstick-web" 
---
+++ src/main/java/com/shopstick/web/ShopstickWebApplication.java
@@ -0,0 +1,13 @@
+package com.shopstick.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopstickWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopstickWebApplication.class, args);
+	}
+
+}
